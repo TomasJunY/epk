396443fce4a5ee357fb7f846384512ab89012a05
Tomáš Junas <junastomas@gmail.com>
2018-03-11 20:04:12 +0100

    dokoncenie usera a pridanie admina

---
+++ src/epk/Administrator.java
@@ -0,0 +1,12 @@
+package epk;
+
+public class Administrator extends User {
+
+	public Administrator(String username, String password) {
+		super(username, password);
+		// TODO Auto-generated constructor stub
+
+	}
+
+
+}
--- src/epk/User.java
+++ src/epk/User.java
@@ -2,16 +2,31 @@ package epk;
 
 public class User {
 	
-	private String username;
-	private String password;
-	private String name;
-	private String surname;
-	private String gender;
-	private int age;
-	private String position;
+	protected String username;
+	protected String password;
+	protected String name;
+	protected String surname;
+	protected String gender;
+	protected int age;
+	protected String position;
 	
 	//konstuktor
-	public 
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	//set info
+	public void setInfo(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+	public void setInfo(String name, String surname, String gender, int age, String position) {
+		this.name = name;
+		this.surname = surname;
+		this.gender = gender;
+		this.age = age;
+		this.position = position;
+	}
 
 }
 
