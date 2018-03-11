f26058fbf53b77dd7818fbd2fc7248101b435c3f
Tomáš Junas <junastomas@gmail.com>
2018-03-11 19:54:25 +0100

    uvodny commit usera

---
+++ src/epk/User.java
@@ -0,0 +1,17 @@
+package epk;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private String name;
+	private String surname;
+	private String gender;
+	private int age;
+	private String position;
+	
+	//konstuktor
+	public 
+
+}
+
