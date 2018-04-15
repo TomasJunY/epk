923eba420b2de8849b4f32edc0145d5b695ecf88
Tomáš Junas <junastomas@gmail.com>
2018-04-15 11:53:29 +0200

    javadoc 10

--- src/message/ManualTimeMessage.java
+++ src/message/ManualTimeMessage.java
@@ -1,13 +1,33 @@
 package message;
 
+/**
+ * <b>TimeMessage Object</b> <br>
+ * 
+ * Object is storing data about TimeMessage <br>
+ * The Message will close at selected date <br>
+ * Can be closed manually
+ * 
+ * @author Tomáš Junas
+ * @version 1.0
+ */
 public class ManualTimeMessage extends TimeMessage {
-	//zatvori sa sama po case da sa vypnut
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param message message
+	 */
 	public ManualTimeMessage(String message) {
 		super(message);
 	}
 	
+	/**
+	 * Sets seen value 
+	 * 
+	 * @param seen seen
+	 */
 	public void setSeen(boolean seen) {
 		this.seen = seen;
 	}
+	
 }
