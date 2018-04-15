4970634e298c5f7a0cc8611bc84862c24894fc7e
Tomáš Junas <junastomas@gmail.com>
2018-04-15 11:15:09 +0200

    javadoc 5

--- src/courses/Test.java
+++ src/courses/Test.java
@@ -12,59 +12,120 @@ public class Test {
 
 	private String name;
 	private String text;
-	private Question[] questions;
-	
+	private Question[] questions;	
 	private int maxPoints;
 	private int achievedPoints;
 	
-	//konstruktor
+	/**
+	 * Constructor
+	 * 
+	 * @param questionCount number of questions
+	 * @param name name of the Test
+	 * @param text text of the Test
+	 */
 	public Test(int questionCount, String name, String text){
 		this.questions = new Question[questionCount]; //pocet otazok
 		this.name = name;
 		this.text = text;
 	}
 	
+	/**
+	 * Sets information about Test
+	 * 
+	 * @param name name of the Test
+	 * @param text text of the Test
+	 */
 	public void setInfo(String name, String text){
 		this.name = name;
 		this.text = text;
 	}
 	
+	/**
+	 * Returns Test name
+	 * 
+	 * @return name
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * Returns Test text
+	 * 
+	 * @return text
+	 */
 	public String getText() {
 		return this.text;
 	}
 	
+	/**
+	 * Return all Questions of the Test
+	 * 
+	 * @return questions
+	 */
 	public Question[] getAllQuestions() {
 		return this.questions;
 	}
 	
+	/**
+	 * Returns Question from array on position
+	 * 
+	 * @param position position in array
+	 * @return questions[position]
+	 */
 	public Question getQuestion(int position) {
 		return this.questions[position];
 	}
 	
+	/**
+	 * Returns length of Question array
+	 * 
+	 * @return questions.length
+	 */
 	public int getQuestionsLength() {
 		return this.questions.length;
 	}
 	
+	/**
+	 * Sets Question in array on position
+	 * @param position position in array
+	 * @param questions question
+	 */
 	public void setQuestion(int position, Question questions) {
 		this.questions[position] = questions;
 	}
 	
+	/**
+	 * Returns maximum points 
+	 *  
+	 * @return maxPoints
+	 */
 	public int getMaxPoints() {
 		return this.maxPoints;
 	}
 	
+	/**
+	 * Sets maximum points 
+	 * 
+	 * @param maxPoints maximum points 
+	 */
 	public void setMaxPoints(int maxPoints) {
 		this.maxPoints = maxPoints;
 	}
 	
+	/**
+	 * Returns achieved points
+	 * 
+	 * @return achievedPoints
+	 */
 	public int getAchievedPoints() {
 		return this.achievedPoints;
 	}
 	
+	/**
+	 * Sets achievedPoints
+	 * @param achievedPoints achievedPoints
+	 */
 	public void setAchievedPoints(int achievedPoints) {
 		this.achievedPoints = achievedPoints;
 	}
